7812fa0d3c9dfbdeb5c4527cccc548c57b4351b5
hadiasta <hijrihadi@gmail.com>
2025-10-27 15:55:37 +0700

    feat :  add uniqe prisma role schema *add api for create role  for no duplicate data master role

---
+++ src/app/api/v1/auth/login/route.ts
@@ -0,0 +1,55 @@
+import { prisma } from '@/lib/prisma'
+import { comparePassword } from '@/lib/bcrypt'
+import { serializeBigInt } from '@/lib/serializeBigIntToString'
+
+export async function POST(req: Request) {
+  try {
+    const { username, password } = await req.json()
+
+    if (!username || !password) {
+      return Response.json({ error: 'Missing required fields' }, { status: 400 })
+    }
+
+    const existingUser = await prisma.auth.findUnique({
+      where: { username },
+      include: {
+        user: {
+          include: { role: true },
+        },
+      },
+    })
+
+    if (!existingUser) {
+      return Response.json({ error: 'Invalid username or password' }, { status: 401 })
+    }
+
+    const valid = await comparePassword(password, existingUser.password)
+
+    if (!valid) {
+      return Response.json({ error: 'Invalid username or password' }, { status: 401 })
+    }
+
+    // Tentukan pesan tambahan jika role masih null
+    const role = existingUser.user?.role?.role_name || null
+    const role_status = !role ? 'Permission needs to be confirmed by admin' : null
+
+    console.log(existingUser.user?.role?.role_name)
+    return Response.json(
+      {
+        message: 'Login successful',
+        data: {
+          id: serializeBigInt(existingUser.user_id),
+          username: existingUser.username,
+          email: existingUser.user?.email,
+          role: existingUser.user?.role?.role_name,
+          role_status,
+        },
+      },
+      { status: 200 }
+    )
+  } catch (error) {
+    // const message = reason instanceof Error ? reason.message : 'Unexpected error'
+    console.error('Login error:', error)
+    return Response.json({ error: 'Internal server error', status: 500 }, { status: 500 })
+  }
+}
