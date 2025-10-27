83ed0ddf389f534cf7a3d4c8142d491ca8041e77
hadiasta <hijrihadi@gmail.com>
2025-10-27 17:03:56 +0700

    feat : init usereducer for register form

--- src/component/auth/register/RegisterForm.tsx
+++ src/component/auth/register/RegisterForm.tsx
@@ -1,6 +1,40 @@
-import React from 'react'
+'use client'
+import { useReducer, ChangeEvent } from 'react'
+
+interface RegisterForm {
+  name: string
+  email: string
+  phone_number: string
+  username: string
+  password: string
+}
+type Action =
+  | { type: 'reset' }
+  | { type: 'setName'; value: RegisterForm['name'] }
+  | { type: 'setEmail'; value: RegisterForm['email'] }
+  | { type: 'setPhoneNumber'; value: RegisterForm['phone_number'] }
+  | { type: 'setUsername'; value: RegisterForm['username'] }
+  | { type: 'setPassword'; value: RegisterForm['password'] }
+
+const initialState: RegisterForm = { name: '', email: '', phone_number: '', username: '', password:''}
+
+
+function stateReducer(state: RegisterForm, action: Action): RegisterForm {
+  switch (action.type) {
+    case 'reset':
+      return initialState
+    case 'setEmail':
+      return { ...state, email: action.value }
+    case 'setPassword':
+      return { ...state, password: action.value }
+    default:
+      throw new Error('Unknown action')
+  }
+}
 
 const RegisterForm = () => {
+  const [state, dispatch] = useReducer(stateReducer, initialState)
+  
   return (
     <>
       <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
@@ -15,7 +49,7 @@ const RegisterForm = () => {
           >
             <div className="flex flex-col gap-3 ">
               <span className="text-xs">
-                Nama  <span className="text-red-700 text-xs">*</span>
+                Nama <span className="text-red-700 text-xs">*</span>
               </span>
               <input
                 type="email"
