092d2f1ae7e3f8cb216558b08149eb5e3f7e0866
hadiasta <hijrihadi@gmail.com>
2025-10-28 21:06:17 +0700

    feat : reset sidebar from shadcn

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,8 +1,7 @@
 import { Nunito } from 'next/font/google'
 import '@/style/globals.css'
 // import Navbar from '@/components/global/Navbar'
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
-import SidebarApp from '@/component/global/SidebarApp'
+
 
 const nunito = Nunito({
   variable: '--font-nunito',
@@ -17,13 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <>
-      <SidebarProvider>
-        <SidebarApp />
+    
         <main className={`${nunito.variable} antialiased`}>
-          <SidebarTrigger />
+      
           {children}
         </main>
-      </SidebarProvider>
+   
     </>
   )
 }
--- src/component/global/SidebarApp.tsx
+++ src/component/global/SidebarApp.tsx
@@ -1,71 +1,8 @@
-// import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
-// ntar import dan download lucide
-
-import {
-   Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "@/components/ui/sidebar"
-
-
-
-const items = [
-  {
-    title: "Home",
-    url: "#",
-    // icon: Home,
-  },
-  {
-    title: "Inbox",
-    url: "#",
-    // icon: Inbox,
-  },
-  {
-    title: "Calendar",
-    url: "#",
-    // icon: Calendar,
-  },
-  {
-    title: "Search",
-    url: "#",
-    // icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    // icon: Settings,
-  },
-]
+import React from 'react'
 
 const SidebarApp = () => {
-  
   return (
-          <Sidebar>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      {/* <item.icon /> */}
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-    </Sidebar>
+    <div>SidebarApp</div>
   )
 }
 
