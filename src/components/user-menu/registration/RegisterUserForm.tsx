5d480ea02b7db64301140c550f56fecd14c3de3d
hadiasta <hijrihadi@gmail.com>
2025-11-17 19:10:57 +0700

    feat : adjust view on user flow

--- src/components/user-menu/registration/RegisterUserForm.tsx
+++ src/components/user-menu/registration/RegisterUserForm.tsx
@@ -34,50 +34,47 @@ const RegisterUserForm = (props: getMenuBySlugProps) => {
 
   return (
     <>
-    <div className='flex flex-col gap-4'>
-      <div className="flex justify-center font-bold text-greenPrimary">Customer Information</div>
-      <div className="relative">
-        <input
-          id="customerName"
-          type="text"
-          value={customerName}
-          onChange={(e) => setCustomerName(e.target.value)}
-          className={clsx(
-            'peer w-full border rounded-lg px-4 pt-4 pb-2 text-sm outline-none transition-all',
-            'border-gray-300 focus:border-greenPrimary focus:ring-1 focus:ring-greenPrimary'
-          )}
-        />
-        
-        <label
-          htmlFor="customerName"
-          className={clsx(
-            'absolute left-3 bg-white px-1 text-xs transition-all duration-200 text-greenPrimary font-bold',
-            customerName
-              ? '-top-2 left-3 text-[11px] text-greenPrimary'
-              : 'top-3.5 text-PlaceHolderGrey text-sm peer-placeholder-shown:translate-y-1',
-            'peer-focus:-top-2 peer-focus:left-3 peer-focus:text-[11px] peer-focus:text-greenPrimary'
-          )}
-        >
-          Name <span className='text-redDanger'>*</span>
-          
-        </label>
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-center font-bold text-greenPrimary">Customer Information</div>
+        <div className="relative">
+          <input
+            id="customerName"
+            type="text"
+            value={customerName}
+            onChange={(e) => setCustomerName(e.target.value)}
+            className={clsx(
+              'peer w-full border rounded-lg px-4 pt-4 pb-2 text-sm outline-none transition-all',
+              'border-gray-300 focus:border-greenPrimary focus:ring-1 focus:ring-greenPrimary'
+            )}
+          />
 
-    
-      </div>
+          <label
+            htmlFor="customerName"
+            className={clsx(
+              'absolute left-3 bg-white px-1 text-xs transition-all duration-200 text-greenPrimary font-bold',
+              customerName
+                ? '-top-2 left-3 text-[11px] text-greenPrimary'
+                : 'top-3.5 text-PlaceHolderGrey text-sm peer-placeholder-shown:translate-y-1',
+              'peer-focus:-top-2 peer-focus:left-3 peer-focus:text-[11px] peer-focus:text-greenPrimary'
+            )}
+          >
+            Name <span className="text-redDanger">*</span>
+          </label>
+        </div>
 
-      <div className="relative">
-        <label className='text-greenPrimary font-semibold '>Table Number</label>
-        <input
-          disabled
-          id="tableNumber"
-          type="text"
-          className={clsx(
-            'peer w-full border rounded-lg px-4 pt-3 pb-2 text-sm outline-none transition-all text-greenPrimary font-bold bg-gray-100 cursor-not-allowed',
-            'border-gray-300 focus:border-greenPrimary focus:ring-1 focus:ring-greenPrimary'
-          )}
-          value={tableNumber}
-        />
-      </div>
+        <div className="relative">
+          <label className="text-greenPrimary font-semibold ">Table Number</label>
+          <input
+            disabled
+            id="tableNumber"
+            type="text"
+            className={clsx(
+              'peer w-full border rounded-lg px-4 pt-3 pb-2 text-sm outline-none transition-all text-greenPrimary font-bold bg-gray-100 cursor-not-allowed',
+              'border-gray-300 focus:border-greenPrimary focus:ring-1 focus:ring-greenPrimary'
+            )}
+            value={tableNumber}
+          />
+        </div>
       </div>
     </>
   )
--- src/components/user-menu/registration/UserRegistrationPage.tsx
+++ src/components/user-menu/registration/UserRegistrationPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import RegisterUserForm from './RegisterUserForm'
+import PaymentMethod from '../payment-method/PaymentMethod'
 
 
 interface getMenuBySlugProps {
@@ -11,9 +12,12 @@ const UserRegistrationPage = (props: getMenuBySlugProps) => {
     const { slug } = props
 
   return (
+    <div>
     <div className='p-6 '>
       <RegisterUserForm   slug={slug} />
     </div> 
+      <PaymentMethod/>
+    </div>
   )
 }
 
